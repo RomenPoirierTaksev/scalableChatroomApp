b2a181d8178a15e0ba0d0fc5b2c735d8584fdfa0
Romen Poirier Taksev <romenpt@hotmail.com>
2022-09-01 13:58:26 -0400

    refactored redis

---
+++ server.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import http from 'http';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { WebSocketServer } from 'ws';
+import Redis from 'ioredis';
+import { v4 as uuid } from 'uuid';
+
+const app = express();
+const server = http.createServer(app);
+const httpServer = http.createServer();
+httpServer.listen(8080, () => console.log("Listening on 8080"))
+const wss = new WebSocketServer({ server: httpServer });
+const redisClusterInfoList = ["chatroomappredisinstance.vjgbsq.clustercfg.use1.cache.amazonaws.com", 6379];
+const redis = await new Redis.Cluster(redisClusterInfoList);
+
+wss.on('connection', (socket) => {
+    socket.id = uuid();
+    redis.publish("connection", socket.id);
+    socket.onmessage = (event) => {
+        const payload = JSON.stringify({ data: event.data, socket: socket.id });
+        redis.publish("WSSMessageSend", payload);
+    }
+});
+
+redis.subscribe("RedisServerSend", "messageList", (err, count) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Subscribed successfully! Currently subbed to ${count} channels`);
+    }
+});
+
+redis.on("message", (channel, message) => {
+    if (channel === "RedisServerSend") {
+        const { data, socket } = JSON.parse(message);
+        [...wss.clients].forEach(c => {
+            if (c.id == socket) {
+                return
+            }
+            c.send(data);
+        });
+    };
+    if (channel === "messageList") {
+        const { messageList, socket } = JSON.parse(message);
+        [...wss.clients].forEach(c => {
+            if (c.id == socket) {
+                for (let i = messageList.length - 1; i >= 0; i--) {
+                    c.send(messageList[i]);
+                }
+            }
+        });
+    };
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(path.join(`${__dirname}/public`)));
+const port = 3000;
+server.listen(port, () => console.log(`Listening on port ${port}`));
